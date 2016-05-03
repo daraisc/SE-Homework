b1d97a295f8c8a4cc1d8a0fd3a5df6e91cb4efae
daraisc <roman.knaute@gmx.de>
2016-05-03 17:48:32 +0200

    Extracting Frequent Renter Points
    Extract Method

--- src/code/Rental.java
+++ src/code/Rental.java
@@ -32,4 +32,12 @@ class Rental {
         }
         return thisAmount;
     }
+	int getFrequentRenterPoints() {
+		// add frequent renter points
+		int frequentRenterPoints =1;;
+		// add bonus for a two day new release rental
+		if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1) 
+		    frequentRenterPoints ++;
+		return frequentRenterPoints;
+	}
 }
